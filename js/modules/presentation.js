62274307f8d785101deb21d2f55c9e938d4c7cc2
Cristian <crdcaro01@gmail.com>
2023-07-04 17:14:21 -0500

    page initial

---
+++ js/modules/presentation.js
@@ -0,0 +1,29 @@
+
+async function getCards() {
+    try {
+      const response = await fetch('../../API/carsd.json');
+      const cards = await response.json();
+      getTemplateCards(cards);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  /**
+   * Hace una petición al archivo handlebars y lo compila
+   */
+  async function getTemplateCards(cards) {
+    try {
+      const response = await fetch( 'templates/cards.hbs');
+      const templateCards = await response.text();
+  
+      const template = Handlebars.compile(templateCards);
+      const htmlCards = cards.map((producto) => template(producto)).join('');
+      document.getElementById('products-container').innerHTML = htmlCards;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  getCards();
+  
